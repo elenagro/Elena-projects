931b8c2582b7ba33cc8dd70138850990cf0b6b29
elenagro <ele.vasevska@gmail.com>
2023-04-04 11:48:28 +0200

    connected to the server

--- api/pkgs/db/index.js
+++ api/pkgs/db/index.js
@@ -9,8 +9,3 @@ const init = () => {
 module.exports = {
   init,
 };
-
-// const init = () => {
-//     const dsn = 'mongodb+srv://zhivko:xx5pAIg0QFcszlrS@cluster0.mcazydz.mongodb.net/Movies?retryWrites=true&w=majority'
-//     mongoose.connect(dsn);
-// };
