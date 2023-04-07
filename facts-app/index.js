4fa09805ac294f25899b42da255b94d4b0645a2b
elenagro <ele.vasevska@gmail.com>
2023-04-07 12:24:26 +0200

    btn open, hide the form

--- facts-app/index.js
+++ facts-app/index.js
@@ -1,3 +1,16 @@
+const btn = document.querySelector(".btn-open");
+const form = document.querySelector(".fact-form");
+
+btn.addEventListener("click", function () {
+  if (form.classList.contains("hidden")) {
+    form.classList.remove("hidden");
+    btn.textContent = "Close";
+  } else {
+    form.classList.add("hidden");
+    btn.textContent = "Share a fact";
+  }
+});
+
 const copyright = document.querySelector(".copyright");
 
 let year = new Date().getFullYear();
