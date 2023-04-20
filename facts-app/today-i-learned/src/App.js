2be9fb44f9e9d5eca5a7d250369b89804ab31132
elenagro <ele.vasevska@gmail.com>
2023-04-20 15:56:47 +0200

    upload supabase

--- facts-app/today-i-learned/src/App.js
+++ facts-app/today-i-learned/src/App.js
@@ -14,69 +14,40 @@ const CATEGORIES = [
   { name: "news", color: "#8b5cf6" },
 ];
 
-const initialFacts = [
-  {
-    id: 1,
-    text: "React is being developed by Meta (formerly facebook)",
-    source: "https://opensource.fb.com/",
-    category: "technology",
-    votesInteresting: 24,
-    votesMindblowing: 9,
-    votesFalse: 4,
-    createdIn: 2021,
-  },
-  {
-    id: 2,
-    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
-    source:
-      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
-    category: "society",
-    votesInteresting: 11,
-    votesMindblowing: 2,
-    votesFalse: 0,
-    createdIn: 2019,
-  },
-  {
-    id: 3,
-    text: "Lisbon is the capital of Portugal",
-    source: "https://en.wikipedia.org/wiki/Lisbon",
-    category: "society",
-    votesInteresting: 8,
-    votesMindblowing: 3,
-    votesFalse: 1,
-    createdIn: 2015,
-  },
-];
-
 const App = () => {
   const [visible, setVisible] = useState(false);
   const [facts, setFacts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCategory, setCurrentCategory] = useState("all");
 
   useEffect(() => {
     const getFacts = async () => {
       setIsLoading(true);
-      const { data: facts, error } = await supabase
-        .from("facts")
-        .select("*")
+
+      let query = supabase.from("facts").select("*");
+
+      if (currentCategory !== "all")
+        query = query.eq("category", currentCategory);
+
+      const { data: facts, error } = await query
+
         .order("votesInteresting", { ascending: false })
-        .limit(10000);
+        .limit(1000);
       setFacts(facts);
-      console.log(facts);
-      console.log(error);
+
       if (!error) setFacts(facts);
       else alert("There was a problem getting the data...");
       setIsLoading(false);
     };
     getFacts();
-  }, []);
+  }, [currentCategory]);
 
   return (
     <>
       <Header visible={visible} setVisible={setVisible} />
       {visible && <NewFactForm setFacts={setFacts} setVisible={setVisible} />}
       <main className="main">
-        <CategoryFilter />
+        <CategoryFilter setCurrentCategory={setCurrentCategory} />
         {isLoading ? (
           <Loader />
         ) : (
@@ -127,25 +98,35 @@ const NewFactForm = ({ setFacts, setVisible }) => {
   const [text, setText] = useState("");
   const [source, setSource] = useState("");
   const [category, setCategory] = useState("");
+  const [isUploading, setIsUploading] = useState(false);
   const textLength = text.length;
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     // valid data
     if (text && isValidHttpUrl(source) && category && textLength <= 200) {
-      // object
-      const newFact = {
-        id: Math.random().toString(),
-        text,
-        source,
-        category,
-        votesInteresting: 0,
-        votesMindblowing: 0,
-        votesFalse: 0,
-        createdIn: new Date().getFullYear(),
-      };
+      // 3. object
+      // const newFact = {
+      //   id: Math.random().toString(),
+      //   text,
+      //   source,
+      //   category,
+      //   votesInteresting: 0,
+      //   votesMindblowing: 0,
+      //   votesFalse: 0,
+      //   createdIn: new Date().getFullYear(),
+      // };
+      // 3. Upload the fact to the Supabase and receive the new fact object
+      setIsUploading(true);
+      const { data: newFact, error } = await supabase
+        .from("facts")
+        .insert([{ text, source, category }])
+        .select();
+      setIsUploading(false);
+
+      console.log(newFact);
       // adding new fact
-      setFacts((prevFacts) => [newFact, ...prevFacts]);
+      if (!error) setFacts((prevFacts) => [newFact[0], ...prevFacts]);
       // reset input fields
       setText("");
       setSource("");
@@ -162,6 +143,7 @@ const NewFactForm = ({ setFacts, setVisible }) => {
         placeholder="Share a fact with the world..."
         value={text}
         onChange={(e) => setText(e.target.value)}
+        disabled={setIsUploading}
       />
       <span>{200 - textLength}</span>
       <input
@@ -169,8 +151,13 @@ const NewFactForm = ({ setFacts, setVisible }) => {
         type="text"
         placeholder="Trustworthy source..."
         onChange={(e) => setSource(e.target.value)}
+        disabled={setIsUploading}
       />
-      <select value={category} onChange={(e) => setCategory(e.target.value)}>
+      <select
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+        disabled={setIsUploading}
+      >
         <option value="">Choose category:</option>
         {CATEGORIES.map((cat) => (
           <option key={cat.name} value={cat.name}>
@@ -178,23 +165,31 @@ const NewFactForm = ({ setFacts, setVisible }) => {
           </option>
         ))}
       </select>
-      <button className="btn btn-large">Post</button>
+      <button className="btn btn-large" disabled={setIsUploading}>
+        Post
+      </button>
     </form>
   );
 };
 
-const CategoryFilter = () => {
+const CategoryFilter = ({ setCurrentCategory }) => {
   return (
     <aside>
       <ul>
         <li className="category">
-          <button className="btn btn-all-categories">All</button>
+          <button
+            onClick={() => setCurrentCategory("all")}
+            className="btn btn-all-categories"
+          >
+            All
+          </button>
         </li>
         {CATEGORIES.map((cat) => (
           <li key={cat.name} className="category">
             <button
               className="btn btn-category"
               style={{ backgroundColor: cat.color }}
+              onClick={() => setCurrentCategory(cat.name)}
             >
               {cat.name}
             </button>
@@ -206,6 +201,13 @@ const CategoryFilter = () => {
 };
 
 const FactList = ({ facts }) => {
+  if (facts.length === 0) {
+    return (
+      <p className="message">
+        No facts for this category yet! Create the first one.
+      </p>
+    );
+  }
   return (
     <section>
       <ul className="facts-list">
