64d51fbe77bc503ab1d309b399d29331a5569a07
Elena <115004783+elenagro@users.noreply.github.com>
2023-05-10 21:04:04 +0200

    Update index.js

--- react-node-app/pkg/db/index.js
+++ react-node-app/pkg/db/index.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const init = () => {
-  const url = process.env.URL;
-  const username = process.env.USERNAME;
-  const password = process.env.PASSWORD;
+  const url = process.env.DB_URL;
+  const username = process.env.DB_USERNAME;
+  const password = process.env.DB_PASSWORD;
   const dbname = process.env.DB_NAME;
   const dsn = `mongodb+srv://${username}:${password}@${url}/${dbname}?retryWrites=true&w=majority`;
   mongoose.connect(dsn);
