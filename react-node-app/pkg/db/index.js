66a617e38606a871b38e7af0eddee7d11579d008
Elena <115004783+elenagro@users.noreply.github.com>
2023-05-10 21:02:58 +0200

    Update dsn

--- react-node-app/pkg/db/index.js
+++ react-node-app/pkg/db/index.js
@@ -8,12 +8,7 @@ const init = () => {
   const password = process.env.PASSWORD;
   const dbname = process.env.DB_NAME;
   const dsn = `mongodb+srv://${username}:${password}@${url}/${dbname}?retryWrites=true&w=majority`;
-  mongoose.connect(dsn, (err) => {
-    if (err) {
-      return console.log("Could not connect to db", err);
-    }
-    console.log("Successfully connetcted to db");
-  });
+  mongoose.connect(dsn);
 };
 
 module.exports = {
